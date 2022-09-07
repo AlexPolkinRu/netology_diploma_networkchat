816981e258a828cba8c3661f31ff60d94e15c116
Aleksandr Polochkin <alex.polkin@gmail.com>
2022-09-07 23:02:36 +0300

    Initial commit

---
+++ src/test/java/server/ServerTest.java
@@ -0,0 +1,43 @@
+package server;
+
+import org.testng.annotations.Test;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+/**
+ * @author Aleksandr Polochkin
+ * 07.09.2022
+ */
+
+public class ServerTest {
+    public static final String SETTINGS_FILENAME = "settings.txt";
+    Server server = new Server();
+    private static final Logger log = Logger.getInstance();
+    String path;
+    String host;
+    int port;
+
+    @Test
+    void main() {
+        try (FileReader reader = new FileReader(SETTINGS_FILENAME)) {
+            Properties props = new Properties();
+            props.load(reader);
+            port = Integer.parseInt(props.getProperty("SERVER_PORT"));
+            host = props.getProperty("SERVER_HOST");
+            path = props.getProperty("SERVER_LOG");
+        } catch (IOException e) {
+            log.log(e.getMessage(), path);
+            System.out.println(e.getMessage());
+        }
+        assertEquals(33333, port);
+        assertEquals("127.0.0.1", host);
+        assertEquals("server.log", path);
+
+    }
+
+
+}
